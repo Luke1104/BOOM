8db051f04ea310488fc4a0ebb2cefee0e6599bb2
DESKTOP-P6QN5A2\admin <2834387127@qq.com>
2020-11-20 09:43:44 +0800

    修改main

--- test01/src/main.js
+++ test01/src/main.js
@@ -3,17 +3,15 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-<<<<<<< HEAD
+
 console.log(1);
 console.log(5555)
-=======
+
 console.log(1)
-<<<<<<< HEAD
+
 console.log(99999)
-=======
+
 console.log(1)
->>>>>>> 2140c88bbf7002868cfd94149bcfc580b5a8b035
->>>>>>> 1eedcfe5b54d1bb25626441e912d4bf1db358d69
 new Vue({
   router,
   render: h => h(App)
