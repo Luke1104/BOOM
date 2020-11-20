0c6c0d92cb323254ccd8180fe368e6b4ecd44858
LuKE <973131311@qq.com>
2020-11-20 10:19:36 +0800

    kk

--- test01/src/main.js
+++ test01/src/main.js
@@ -3,20 +3,9 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-
-console.log(1);
-console.log(5555)
-
-console.log(1)
-
-console.log(99999)
-
 console.log(1)
 console.log(99999)
 console.log(1)
-console.log(8274)
-console.log(12345)
-console.log("最后的倔强")
 new Vue({
   router,
   render: h => h(App)
